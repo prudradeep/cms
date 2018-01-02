15f0c3a46dec2cf8d304f53448a8983321837a63
Pradeep Raput <prithviraj.rudraksh@gmail.com>
2018-01-02 23:29:12 +0530

    Update users.sql

--- components/user/sql/users.sql
+++ components/user/sql/users.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) NOT NULL,
   `usertype` int(11) NOT NULL DEFAULT '1',
   `status` tinyint(4) NOT NULL DEFAULT '1',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`username`),
   KEY `usertype` (`usertype`),
